3f772ac4614c6b275c9a6d023188de1caad90fc3
Binhdoe <binhk12000@yahoo.de>
2017-12-05 12:54:08 +0100

    Home Index, doppelsuche eingebaut, nur eins kann jedoch nur gleichzeitig gesucht werden

--- BibApp/BibApp/Controllers/HomeController.cs
+++ BibApp/BibApp/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Dynamic;
 using System.Collections.Generic;
+using System;
 
 namespace BibApp.Controllers
 {
@@ -18,12 +19,110 @@ namespace BibApp.Controllers
             _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string sortOrder, string searchString, string searchString2)
         {
             HomeIndexData model = new HomeIndexData();
 
-            model.Benutzers = _context.Benutzers.ToList();
-            model.Buecher = _context.Buecher.ToList();
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["IdSortParm"] = sortOrder == "Id" ? "id_desc" : "Id";
+            ViewData["AutSortParm"] = sortOrder == "Autor" ? "aut_desc" : "Autor";
+            ViewData["entlBisSortParm"] = sortOrder == "EntliehenBis" ? "entlBis_desc" : "EntliehenBis";
+            ViewData["entlVomSortParm"] = sortOrder == "EntliehenVom" ? "entlVom_desc" : "EntliehenVom";
+            ViewData["VorgSortParm"] = sortOrder == "Vorgemerkt" ? "vorg_desc" : "Vorgemerkt";
+            ViewData["VerfSortParm"] = sortOrder == "Verfügbarkeit" ? "verfüg_desc" : "Verfügbarkeit";
+            ViewData["VerlagSortParm"] = sortOrder == "Verlag" ? "verlag_desc" : "Verlag";
+            ViewData["CurrentFilter"] = searchString;
+
+            var books = from s in _context.Buecher
+                        select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                books = books.Where(s =>
+                s.Bezeichnung.Contains(searchString)
+                || s.Autoren.Contains(searchString)
+                || s.Verlag.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "id_desc":
+                    books = books.OrderByDescending(s => s.Id);
+                    break;
+                case "Id":
+                    books = books.OrderBy(s => s.Id);
+                    break;
+                case "aut_desc":
+                    books = books.OrderByDescending(s => s.Autoren);
+                    break;
+                case "Autor":
+                    books = books.OrderBy(s => s.Autoren);
+                    break;
+                case "name_desc":
+                    books = books.OrderByDescending(s => s.Bezeichnung);
+                    break;
+                case "entlBis_desc":
+                    books = books.OrderByDescending(s => s.EntliehenBis);
+                    break;
+                case "EntliehenBis":
+                    books = books.OrderBy(s => s.EntliehenBis);
+                    break;
+                case "entlVom_desc":
+                    books = books.OrderByDescending(s => s.EntliehenVom);
+                    break;
+                case "EntliehenVom":
+                    books = books.OrderBy(s => s.EntliehenVom);
+                    break;
+                case "vorg_desc":
+                    books = books.OrderByDescending(s => s.IstVorgemerkt);
+                    break;
+                case "Vorgemerkt":
+                    books = books.OrderBy(s => s.IstVorgemerkt);
+                    break;
+                case "verfüg_desc":
+                    books = books.OrderByDescending(s => s.Verfügbarkeit);
+                    break;
+                case "Verfügbarkeit":
+                    books = books.OrderBy(s => s.Verfügbarkeit);
+                    break;
+                case "verlag_desc":
+                    books = books.OrderByDescending(s => s.Verlag);
+                    break;
+                case "Verlag":
+                    books = books.OrderBy(s => s.Verlag);
+                    break;
+                default:
+                    books = books.OrderBy(s => s.Bezeichnung);
+                    break;
+            }
+
+            ViewData["NameSortParm2"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["RoleSortParm2"] = sortOrder == "Role" ? "role_desc" : "Role";
+            ViewData["CurrentFilter2"] = searchString2;
+
+            var benutzer = from s in _context.Benutzers
+                           select s;
+
+            if (!String.IsNullOrEmpty(searchString2))
+            {
+                benutzer = benutzer.Where(s => s.UserName.Contains(searchString2));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    benutzer = benutzer.OrderByDescending(s => s.UserName);
+                    break;
+                case "role_desc":
+                    benutzer = benutzer.OrderByDescending(s => s.Role);
+                    break;
+                case "Role":
+                    benutzer = benutzer.OrderBy(s => s.Role);
+                    break;
+                default:
+                    benutzer = benutzer.OrderBy(s => s.UserName);
+                    break;
+            }
+
+            model.Benutzers = await benutzer.AsNoTracking().ToListAsync();
+            model.Buecher = await books.AsNoTracking().ToListAsync();
             return View(model);
         }
 
