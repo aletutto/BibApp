8639b1fd4c4b11579a123fdaf9e03ada0c40dd5c
Alessandro <Alessandro@TUTTOBENE>
2018-01-07 20:07:05 +0100

    Buch Index -> Hover Effekt wieder eingebaut Navbar -> Aktives Element wird hervorgehoben

---
+++ BibApp/BibApp/Models/Shared/Utilities.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BibApp.Models.Shared
+{
+    public static class Utilities
+    {
+        public static string IsActive(
+            this IHtmlHelper html,
+            string control,
+            string action)
+        {
+            var routeData = html.ViewContext.RouteData;
+
+            var routeAction = (string)routeData.Values["action"];
+            var routeControl = (string)routeData.Values["controller"];
+
+            // both must match
+            var returnActive = control == routeControl &&
+                               action == routeAction;
+
+            return returnActive ? "active" : "";
+        }
+    }
+}
